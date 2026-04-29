e467d78b1ea5a3399dd13377982be5517e16a4c4
Jim Edwards <jimbo-github@edwardsclan.net>
2026-04-28 19:58:09 -0700

    Fix typo

--- components/mirage_ventusx/mirage_ventusx.cpp
+++ components/mirage_ventusx/mirage_ventusx.cpp
@@ -40,109 +40,6 @@ const uint8_t VENTUSX_TEMP_OFFSET = 60;
 const uint8_t VENTUSX_B3_BIT_DISPLAY_OFF = 0x02;
 const uint8_t VENTUSX_B3_BIT_UNIT_POWER = 0x20;
 
-/*
-void MirageClimate::transmit_state()
-{
-  this->last_transmit_time_ = millis(); // setting the time of the last transmission.
-  uint8_t remote_state[MIRAGE_STATE_LENGTH] = {0};
-  remote_state[0] = 0x56;
-  remote_state[1] = MIRAGE_TEMP_OFFSET; // Starting temperature
-
-  auto powered_on = this->mode != climate::CLIMATE_MODE_OFF;
-  if (powered_on)
-  {
-    remote_state[5] = 0x1A;
-  }
-
-  switch (this->mode)
-  {
-  case climate::CLIMATE_MODE_HEAT_COOL:
-    remote_state[4] |= MIRAGE_AUTO;
-    break;
-  case climate::CLIMATE_MODE_HEAT:
-    remote_state[4] |= MIRAGE_HEAT;
-    break;
-  case climate::CLIMATE_MODE_COOL:
-    remote_state[4] |= MIRAGE_COOL;
-    break;
-  case climate::CLIMATE_MODE_DRY:
-    remote_state[4] |= MIRAGE_DRY;
-    break;
-  case climate::CLIMATE_MODE_FAN_ONLY:
-    remote_state[4] |= MIRAGE_FAN;
-    break;
-  case climate::CLIMATE_MODE_OFF:
-    remote_state[5] = MIRAGE_POWER_OFF;
-  default:
-    break;
-  }
-
-  // Temperature
-  auto temp = (uint8_t)roundf(clamp(this->target_temperature, float(16), float(32)));
-  remote_state[1] += temp;
-
-  // Fan speed
-  switch (this->fan_mode.value())
-  {
-  case climate::CLIMATE_FAN_LOW:
-    remote_state[4] |= MIRAGE_FAN_LOW;
-    break;
-  case climate::CLIMATE_FAN_MEDIUM:
-    remote_state[4] |= MIRAGE_FAN_MED;
-    break;
-  case climate::CLIMATE_FAN_HIGH:
-    remote_state[4] |= MIRAGE_FAN_HIGH;
-    break;
-  default:
-    break;
-  }
-
-  // Swing
-  if (this->swing_mode == climate::CLIMATE_SWING_VERTICAL || this->swing_mode == climate::CLIMATE_SWING_BOTH)
-  {
-    remote_state[5] |= 0x1A;
-  }
-  if (this->swing_mode == climate::CLIMATE_SWING_HORIZONTAL || this->swing_mode == climate::CLIMATE_SWING_BOTH)
-  {
-    remote_state[5] |= 1;
-  }
-
-  if (this->swing_mode == climate::CLIMATE_SWING_OFF)
-  {
-    if (this->swing_position > 5)
-      this->swing_position = 0;
-    this->swing_position += 1;
-    remote_state[5] |= 2;
-    remote_state[5] |= this->swing_position << 2;
-  }
-
-  ESP_LOGI(TAG,
-           "Sending: %02X %02X %02X %02X   %02X %02X %02X %02X   %02X %02X %02X %02X   %02X %02X",
-           remote_state[0], remote_state[1], remote_state[2], remote_state[3], remote_state[4], remote_state[5],
-           remote_state[6], remote_state[7], remote_state[8], remote_state[9], remote_state[10], remote_state[11],
-           remote_state[12], remote_state[13]);
-
-  // Send code
-  ESP_LOGD(TAG, "TX temp=%d byte5=0x%02X checksum=0x%02X", temp, remote_state[5], remote_state[11]);
-
-  // Send the actual data
-  esphome::remote_base::AEHAData aeha;
-  aeha.address = VENTUSX_ADDRESS;
-  aeha.data.assign(remote_state, remote_state + VENTUSX_STATE_LENGTH);
-
-  // Unit requires the frame sent twice
-  for (int i = 0; i < 2; i++)
-  {
-    auto transmit = this->transmitter_->transmit();
-    auto *tx_data = transmit.get_data();
-    esphome::remote_base::AEHAProtocol().encode(tx_data, aeha);
-    transmit.perform();
-    if (i == 0)
-      delay(40);
-  }
-}
-*/
-
 // Bit-reverse an 8-bit value (e.g. 0b00000001 -> 0b10000000)
 uint8_t MirageVentusXClimate::bit_reverse(uint8_t b)
 {
@@ -164,37 +61,111 @@ uint8_t MirageVentusXClimate::calc_checksum(const uint8_t *data, uint8_t len)
   return bit_reverse(temp);
 }
 
-void MirageVentusXClimate::transmit_state() {
-  uint8_t data[12] = {
+void MirageVentusXClimate::transmit_state()
+{
+  this->last_transmit_time_ = millis(); // setting the time of the last transmission.
+  uint8_t remote_state[MIRAGE_STATE_LENGTH] = {0};
+
+  // Header
+  remote_state[0] = VENTUSX_HEADER_B0;
+  remote_state[1] = VENTUSX_HEADER_B1_DATA;
+  remote_state[2] = VENTUSX_HEADER_B2;
+
+  // Mode setup
+  auto powered_on = this->mode != climate::CLIMATE_MODE_OFF;
+  if (powered_on)
+  {
+    remote_state[3] |= VENTUSX_B3_BIT_UNIT_POWER;
+  }
+
+  switch (this->mode)
+  {
+    case climate::CLIMATE_MODE_HEAT_COOL:
+      remote_state[4] |= VENTUSX_B4_MODE_AUTO;
+      break;
+    case climate::CLIMATE_MODE_HEAT:
+      remote_state[4] |= VENTUSX_B4_MODE_HEAT;
+      break;
+    case climate::CLIMATE_MODE_COOL:
+      remote_state[4] |= VENTUSX_B4_MODE_COOL;
+      break;
+    case climate::CLIMATE_MODE_DRY:
+      remote_state[4] |= VENTUSX_B4_MODE_DRY;
+      break;
+    case climate::CLIMATE_MODE_FAN_ONLY:
+      remote_state[4] |= VENTUSX_B4_MODE_FAN;
+      break;
+     default:
+      break;
+  }
+
+  // Temperature
+  auto temp = (uint8_t)roundf(clamp(this->target_temperature, float(16), float(32)));
+  remote_state[1] += temp;
+
+  // Fan speed
+  switch (this->fan_mode.value())
+  {
+  case climate::CLIMATE_FAN_LOW:
+    remote_state[6] |= VENTUSX_B6_FAN_LOW;
+    break;
+  case climate::CLIMATE_FAN_MEDIUM:
+    remote_state[6] |= VENTUSX_B6_FAN_MID;
+    break;
+  case climate::CLIMATE_FAN_HIGH:
+    remote_state[6] |= VENTUSX_B6_FAN_HIGH;
+    break;
+  case climate::CLIMATE_FAN_AUTO:
+  default:
+    remote_state[6] |= VENTUSX_B6_FAN_AUTO;
+    break;
+  }
+
+  // Swing 
+  if (this->swing_mode == climate::CLIMATE_SWING_VERTICAL || this->swing_mode == climate::CLIMATE_SWING_BOTH)
+  {
+    remote_state[6] |= VENTUSX_B6_SWING_VERT_MASK;
+  }
+  if (this->swing_mode == climate::CLIMATE_SWING_HORIZONTAL || this->swing_mode == climate::CLIMATE_SWING_BOTH)
+  {
+    remote_state[10] |= VENTUSX_B10_SWING_HORIZ_BIT;
+  }
+
+  // Checksum
+  remote_state[11] = calc_checksum(remote_state, VENTUSX_STATE_LENGTH - 1);
+
+  ESP_LOGVV(TAG,
+            "RX VentusX\n  Header: %02X %02X %02X %02X\n  Mode: %02X\n  Temp: %02X\n  Fan: %02X\n  Reserved: %02X %02X %02X\n  Horz Swing: %02X\n  Checksum: %02X",
+            remote_state[0], remote_state[1], remote_state[2], remote_state[3], remote_state[4], remote_state[5],
+            remote_state[6], remote_state[7], remote_state[8], remote_stated[9], remote_state[10], remote_state[11]);
+
+  ESP_LOGD(TAG, "TX temp=%d byte5=0x%02X checksum=0x%02X", temp, remote_state[5], remote_state[11]);
+
+  // Send the wakeup packet (hard-coded)
+  esphome::remote_base::AEHAData aeha_wakeup;
+  aeha.address = VENTUSX_ADDRESS;
+  uint8_t wakeup_packet[12] = {
       0x64, 0x80, 0x00, 0x24,
       0xC0, 0x00, 0x1C, 0x00,
       0x00, 0x00, 0x01, 0x00};
+  }
+  aeha_wakeup.data.assign(wakeup_packet, wakeup_packet + VENTUSX_STATE_LENGTH);
+  auto transmit = this->transmitter_->transmit();
+  auto *tx_data = transmit.get_data();
+  esphome::remote_base::AEHAProtocol().encode(tx_data, aeha_wakeup);
+  transmit.perform();
 
-  uint8_t temp = (uint8_t) this->target_temperature;
-  if (temp < 60) temp = 60;
-  if (temp > 80) temp = 80;
+  delay(40);
 
-  // Confirmed encoding from trace analysis
-  data[5] = ((temp - 60) * 0x10) >> 1;
-
-  uint8_t sum = 0;
-  for (int i = 0; i < 11; i++) sum += data[i];
-  data[11] = sum & 0xFF;
-
-  ESP_LOGD(TAG, "TX temp=%d byte5=0x%02X checksum=0x%02X", temp, data[5], data[11]);
-
+  // Send the actual data
   esphome::remote_base::AEHAData aeha;
   aeha.address = VENTUSX_ADDRESS;
-  aeha.data.assign(data, data + VENTUSX_STATE_LENGTH);
+  aeha.data.assign(remote_state, remote_state + VENTUSX_STATE_LENGTH);
 
-  // Unit requires the frame sent twice
-  for (int i = 0; i < 2; i++) {
-    auto transmit = this->transmitter_->transmit();
-    auto *tx_data = transmit.get_data();
-    esphome::remote_base::AEHAProtocol().encode(tx_data, aeha);
-    transmit.perform();
-    if (i == 0) delay(40);
-  }
+  transmit = this->transmitter_->transmit();
+  tx_data = transmit.get_data();
+  esphome::remote_base::AEHAProtocol().encode(tx_data, aeha);
+  transmit.perform();
 }
 
 bool MirageVentusXClimate::on_receive(remote_base::RemoteReceiveData data) {
